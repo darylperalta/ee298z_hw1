e134c22117f217d7d6b32a2c01d40b86daf731c2
darylperalta <darylperalta93@gmail.com>
2018-02-06 21:22:23 +0800

    adaptive lr

--- linreg.py
+++ linreg.py
@@ -1,6 +1,10 @@
 import sys
 import numpy as np
 
+#hyperparameters
+lr = 0.01
+tol = 1
+
 coeff = sys.argv[1:]
 coeff = list(map(float,coeff))
 
@@ -13,8 +17,8 @@ print(coeff)
 
 #convert to Numpy array the coefficients
 x = np.array(coeff)
-print("x")
-print(x)
+#print("x")
+#print(x)
 x = x.reshape((-1,1))
 
 #create input vector
@@ -22,27 +26,27 @@ num_train = 1000
 val = np.array(list(range(-num_train//2,num_train//2)))
 val = val/50
 #val = val.reshape((-1,1))
-print("val")
-print(val)
-print(val.shape)
+#print("val")
+#print(val)
+#print(val.shape)
 
 A = np.zeros((num_train,degree+1))
 #create A Matrix
 for i in range(degree):
     A[:,i] = val**(degree-i)
 A[:,degree] = np.ones((num_train))
-print("A")
-print(A.shape)
-print(A)
+#print("A")
+#print(A.shape)
+#print(A)
 #Solve for b
 b = np.matmul(A,x)
-print("b")
-print(b)
+#print("b")
+#print(b)
 
 #create noise
-noise = np.random.uniform(low=-0.5,high=0.5,size=(num_train,1))
-print("noise")
-print(noise)
+noise = np.random.uniform(low=-1,high=1,size=(num_train,1))
+#print("noise")
+#print(noise)
 #A_noise= noise+A
 #print("A with noise")
 #print(A_noise)
@@ -54,24 +58,23 @@ loss = np.mean((np.matmul(A,x)-b_noise)**2)
 #print("loss")
 #print(loss)
 
-lr = 0.00001
-tol = 3
+
 #print(np.matmul(A_noise.T,b))
 #print(np.matmul(A_noise.T,np.matmul(A_noise,x)))
 grad = np.matmul(A.T,np.matmul(A,x))-np.matmul(A.T,b_noise)
-print("initial grad")
-print(grad)
+#print("initial grad")
+#print(grad)
 #print(np.matmul(A.T,np.matmul(A,x))-np.matmul(A.T,b))
 #print(np.sqrt(np.sum(grad**2)))
 #x_solved = x+np.random.uniform(low=0,high=1,size=x.shape)
 x_solved =np.random.uniform(low=0,high=0.1,size=x.shape)
 #x_solved = x
-print("initial x")
-print(x_solved)
+#print("initial x")
+#print(x_solved)
 #print(np.sqrt(np.mean(grad**2)))
-print("init loss")
-loss = np.mean((np.matmul(A,x_solved)-b_noise)**2)
-print(loss)
+#print("init loss")
+loss1 = np.mean((np.matmul(A,x_solved)-b_noise)**2)
+#print(loss)
 iter_ctr = 0
 lr_ctr = 1
 #print()
@@ -79,6 +82,10 @@ lr_ctr = 1
 
 while(np.linalg.norm(grad,2)>tol):
 #while(loss>tol):
+    loss2 = np.mean((np.matmul(A,x_solved)-b_noise)**2)
+    if(loss2>loss1):
+        lr = lr/5
+        loss1=loss2
     grad = np.matmul(np.matmul(A.T,A),x_solved)-np.matmul(A.T,b_noise)
 
     #print(grad)
@@ -91,7 +98,7 @@ while(np.linalg.norm(grad,2)>tol):
     #print("x")
     #print(x_solved)
     #print(np.sqrt(np.mean(grad**2)))
-    print("grad norm", np.linalg.norm(grad,2), "loss", loss, "x", x_solved.flatten(), "iter", iter_ctr)
+    print("grad norm", np.linalg.norm(grad,2), "loss", loss, "x", x_solved.flatten(),"lr",lr, "iter", iter_ctr)
     #print(np.linalg.norm(grad,2))
     #print(iter_ctr)
     iter_ctr += 1
